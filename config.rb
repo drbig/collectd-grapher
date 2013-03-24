94bb347fcd948a37000bed23077518e1525ea3a9
Piotr S. Staszewski <p.staszewski@gmail.com>
2013-03-24 11:36:55 +0100

    Add simple automated structure checking to config

--- config.rb
+++ config.rb
@@ -1,3 +1,4 @@
+#!/usr/bin/env ruby
 # coding: utf-8
 #
 
@@ -162,3 +163,40 @@ PLUGINS = {
     },
   },
 }
+
+if __FILE__ == $0
+  def die(msg)
+    STDERR.puts msg
+    exit(2)
+  end
+
+  def has_all(info, container, keys)
+    keys.each do |k|
+      die("#{info} doesn't have #{k}") unless container.member? k
+    end
+  end
+
+  die('RRD_PATH not a directory') unless (File.exists? RRD_PATH and File.directory? RRD_PATH)
+  die('GRAPH_PATH not a directory') unless (File.exists? GRAPH_PATH and File.directory? GRAPH_PATH)
+  die('CONTRAST has to be 0 <= x <= 16') unless (CONTRAST >= 0 and CONTRAST <= 16)
+  has_all('AGGREGATOR LABELS', AGGREGATOR_LABELS, [:min, :max, :average])
+  die('RESOLUTIONS has to have at least defined') if RESOLUTIONS.empty?
+
+  PLUGINS.each_pair do |k, c|
+    die("PLUGIN #{k} has no charts defined") if c.empty?
+    c.each_pair do |n, c|
+      where = "PLUGIN #{k} CHART #{n}"
+      has_all(where, c, [:y_axis_title, :prefix, :chart, :order, :pallette])
+      die("#{where} order.length != pallette.length") unless c[:order].length == c[:pallette].length
+      if c.has_key? :titles
+        die("#{where} order.length != titles.length") unless c[:order].length == c[:titles].length
+      end
+      if c.has_key? :ds_names
+        die("#{where} order.length != ds_names.length") unless c[:order].length == c[:ds_names].length
+      end
+    end
+  end
+
+  puts 'Looks okay overall.'
+  exit(0)
+end
